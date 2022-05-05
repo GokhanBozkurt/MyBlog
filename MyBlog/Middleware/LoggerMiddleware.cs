61c714db29a33848113359e714c2d9a7f7965083
is96692 <gokhan.bozkurt@softtech.com.tr>
2022-05-05 12:03:56 +0300

    kickoff Middlewre

---
+++ MyBlog/Middleware/LoggerMiddleware.cs
@@ -0,0 +1,20 @@
+﻿namespace Middleware
+{
+    public class LoggerMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public LoggerMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext context )
+        {
+            Console.WriteLine("LoggerMiddleware");
+            Console.WriteLine(context.Request.Path);
+
+            await next.Invoke(context);
+        }
+    }
+}
