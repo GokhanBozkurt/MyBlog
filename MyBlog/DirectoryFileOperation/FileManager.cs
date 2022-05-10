3e64351b98a747bed16e4a5471525716daa29787
is96692 <gokhan.bozkurt@softtech.com.tr>
2022-05-10 14:43:35 +0300

    DirectoryFileOperation

---
+++ MyBlog/DirectoryFileOperation/FileManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryFileOperation
+{
+    public class FileManager
+    {
+        public static void ReadFilesFromDirectory(string path, string searchPattern)
+        {
+            DirectoryInfo di = new DirectoryInfo(path);
+            FileInfo[] files = di.GetFiles();
+
+            foreach (var item in files)
+            {
+                Console.WriteLine(item.Name );
+            }
+        }
+    }
+}
