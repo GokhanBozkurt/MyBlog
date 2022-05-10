11e0e90b3baa3d87f5005e28751b492f9aab6665
is96692 <gokhan.bozkurt@softtech.com.tr>
2022-05-10 14:41:39 +0300

    Stopwatch

---
+++ MyBlog/Stopwatch/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+Console.WriteLine("Stopwatch starting ");
+
+var stopwatch = new Stopwatch();
+
+stopwatch.Start();
+
+Thread.Sleep(10000);
+
+stopwatch.Stop();
+
+TimeSpan elapsedTime = stopwatch.Elapsed;
+
+Console.WriteLine("Stopwatch elapsedTime " + elapsedTime.ToString(@"m\:ss\.fff"));
+
+Console.ReadLine();
+
